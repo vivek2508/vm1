517457bee29c3eec8763eddc1030d8d855ec3f0d
vivek2508 <103503097+vivek2508@users.noreply.github.com>
2022-04-21 15:28:20 +0530

    Update main.tf

--- vm/main.tf
+++ vm/main.tf
@@ -102,7 +102,7 @@ resource "azurerm_linux_virtual_machine" "myterraformvm" {
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.myterraformnic.id]
-  size                  = "Standard_DS1_v2"
+  size                  = "Standard_D2ds_v5"
   zone                  = 1
   
 

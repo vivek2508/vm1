6a068e3c1d201142a448c2c0e76ad62b5a1d852e
vivek2508 <103503097+vivek2508@users.noreply.github.com>
2022-04-21 16:38:21 +0530

    Update variable.tf

--- vm/variable.tf
+++ vm/variable.tf
@@ -4,6 +4,6 @@ variable "resource_group_name_prefix" {
 }
 
 variable "resource_group_location" {
-  default       = "eastus"
+  default       = "westeurope"
   description   = "Location of the resource group."
 }

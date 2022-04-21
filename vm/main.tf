6cbd237628119f1213afe0ccb1f6eb635a96657f
vivek2508 <103503097+vivek2508@users.noreply.github.com>
2022-04-21 17:08:18 +0530

    Update main.tf

--- vm/main.tf
+++ vm/main.tf
@@ -93,7 +93,7 @@ resource "azurerm_linux_virtual_machine" "myterraformvm" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "20.04-LTS"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
 

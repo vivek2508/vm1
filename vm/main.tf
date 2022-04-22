685b058f2dcaec5e39c322987875a83c072df34e
vivek2508 <103503097+vivek2508@users.noreply.github.com>
2022-04-22 11:06:27 +0530

    Update main.tf

--- vm/main.tf
+++ vm/main.tf
@@ -60,7 +60,7 @@ resource "azurerm_network_interface" "myterraformnic" {
   ip_configuration {
     name                          = "myNicConfiguration"
     subnet_id                     = azurerm_subnet.myterraformsubnet.id
-    private_ip_address_allocation = "Dynamic"
+    private_ip_address_allocation = "Static"
     public_ip_address_id          = azurerm_public_ip.myterraformpublicip.id
   }
 }

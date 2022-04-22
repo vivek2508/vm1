614e23bfb2048dcea98ed221f9629eaecb955ebc
vivek2508 <103503097+vivek2508@users.noreply.github.com>
2022-04-22 18:00:10 +0530

    Update main.tf

--- vm/main.tf
+++ vm/main.tf
@@ -69,6 +69,30 @@ resource "azurerm_network_interface_security_group_association" "example" {
   network_interface_id      = azurerm_network_interface.myterraformnic.id
   network_security_group_id = azurerm_network_security_group.myterraformnsg.id
 }
+# Generate random text for a unique storage account name
+resource "random_id" "randomId" {
+  keepers = {
+    # Generate a new ID only when a new resource group is defined
+    resource_group = azurerm_resource_group.rg.name
+  }
+
+  byte_length = 8
+}
+
+# Create storage account for boot diagnostics
+resource "azurerm_storage_account" "mystorageaccount" {
+  name                     = "diag${random_id.randomId.hex}"
+  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+# Create (and display) an SSH key
+resource "tls_private_key" "example_ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
 
 # Create virtual machine
 resource "azurerm_linux_virtual_machine" "myterraformvm" {
@@ -96,6 +120,15 @@ resource "azurerm_linux_virtual_machine" "myterraformvm" {
   admin_username                  = "azureuser"
   disable_password_authentication = false
   admin_password                  = "Password@321"
+  
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = tls_private_key.example_ssh.public_key_openssh
+  }
+
+  boot_diagnostics {
+    storage_account_uri = azurerm_storage_account.mystorageaccount.primary_blob_endpoint
+  }
 
 
   

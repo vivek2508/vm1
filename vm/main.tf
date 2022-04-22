0cdfdb3c193255be416e3b2c7fbf22f7f15f9df3
vivek2508 <103503097+vivek2508@users.noreply.github.com>
2022-04-22 10:38:01 +0530

    Update main.tf

--- vm/main.tf
+++ vm/main.tf
@@ -78,7 +78,7 @@ resource "azurerm_linux_virtual_machine" "myterraformvm" {
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.myterraformnic.id]
-  size                  = "Standard_D4"
+  size                  = "Standard_D2s_v3"
   zone                  = 2
 #   generation            = 2
   
@@ -94,7 +94,7 @@ resource "azurerm_linux_virtual_machine" "myterraformvm" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "18.04-lts"
+    sku       = "18.04-lts-gen2"
     version   = "latest"
     
   }

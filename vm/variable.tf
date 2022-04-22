8150085c799b41aa5073bfdd9fb7bf3afd517944
vivek2508 <103503097+vivek2508@users.noreply.github.com>
2022-04-22 17:57:50 +0530

    Update variable.tf

--- vm/variable.tf
+++ vm/variable.tf
@@ -7,16 +7,4 @@ variable "resource_group_location" {
   default       = "australiaeast"
   description   = "Location of the resource group."
 }
-variable "random_password_length" {
-  description = "The desired length of random password created by this module"
-  type        = number
-  default     = 24
-}
-variable "rbac" {
-  default = []
-  type = list(object({
-    principal_id = string
-    role         = string
-  }))
-  description = "Role assignments for principals on the VM."
-}
+
